bcd446ab4a5cd5b0623673ca7b03d7fd7c8a4858
Mikhail Efremov <mikovich.mike@gmail.com>
2022-11-28 17:06:59 +0300

    завершена логика для консольной версии игры

--- tic-tac-toe-cli/tic-tac-toe/Game.cs
+++ tic-tac-toe-cli/tic-tac-toe/Game.cs
@@ -13,9 +13,9 @@ namespace tic_tac_toe
 		private enum GameStates
 		{
 			Playing,
-			Draw,
-			X_Won,
-			O_Won
+			Ended_With_Draw,
+			Ended_With_X_Win,
+			Ended_With_O_Win
 		}
 
 		private enum Players
@@ -36,8 +36,7 @@ namespace tic_tac_toe
 
 		#region Поля
 
-		private int[][] gameField;
-		private int[] xCells, oCells;
+		private Cell[][] gameField;
 
 		private GameStates state;
 		private Players currentPlayer;
@@ -49,8 +48,7 @@ namespace tic_tac_toe
 
 		private struct Cell
 		{
-			private int x;
-			private int y;
+			private int x, y;
 			private Players value;
 
 			public int X { get => x; }
@@ -74,6 +72,27 @@ namespace tic_tac_toe
 
 				return false;
 			}
+
+			public static bool operator ==(Cell first, Cell second)
+			{
+				if (first.X == second.X && first.Y == second.Y && first.Value == second.Value)
+				{
+					return true;
+				}
+
+				return false;
+			}
+
+			public static bool operator !=(Cell first, Cell second)
+			{
+				if (first.X == second.X && first.Y == second.Y && first.Value == second.Value)
+				{
+					return false;
+				}
+
+				return true;
+			}
+
 		}
 
 		#endregion
@@ -90,20 +109,18 @@ namespace tic_tac_toe
 		/// </summary>
 		private void GenerateNewGame()
 		{
-			this.gameField = new int[3][]
+
+			this.gameField = new Cell[3][]
 									{
-										new int[] {0,0,0},
-										new int[] {0,0,0},
-										new int[] {0,0,0}
+										new Cell[3] {new Cell(0,0,Players.N),new Cell(0,1,Players.N),new Cell(0,2,Players.N)},
+										new Cell[3] {new Cell(1,0,Players.N),new Cell(1,1,Players.N),new Cell(1,2,Players.N)},
+										new Cell[3] {new Cell(2,0,Players.N),new Cell(2,1,Players.N),new Cell(2,2,Players.N)}
 									};
 
 			this.state = GameStates.Playing;
 			this.currentPlayer = Players.X;
 			this.winner = Players.N;
 			this.AvailableCellsList = new List<Cell>(9);
-
-			this.xCells = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-			this.oCells = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 			InitializeAvailableCells();
 
@@ -116,6 +133,7 @@ namespace tic_tac_toe
 		{
 			while (state == GameStates.Playing)
 			{
+				Console.Clear();
 				//Выводим текущее игровое поле
 				PrintGameField();
 
@@ -123,30 +141,62 @@ namespace tic_tac_toe
 				string current = this.currentPlayer.ToString();
 
 				//Просим игрока ввести координаты ячейки
-				Console.WriteLine($"Ход игрока {current}");
-				Console.WriteLine($"Введите координаты ячейки (в формате X,Y, где 0,0 - левый верхний угол), в которую желаете поставить {current}");
+				Console.WriteLine("\n");
+				Console.WriteLine($"Ход игрока: {current}");
+				Console.WriteLine("\n");
+				Console.WriteLine($"Введите координаты ячейки (в формате <Строка>,<Столбец>, где 0,0 - левый верхний угол), в которую желаете поставить {current}");
 				Console.WriteLine("Доступны ячейки:");
 				PrintAvailableCells();
 
 				//Считывание ввода с клавиатуры
-				string? _ = Console.ReadLine();
+				string[] coordinates = Console.ReadLine().Split(',');
 
-				//Обновление доступных ячеек
-				UpdateAvailableCells();
+				try
+				{
+					Cell myCell = new Cell(Convert.ToInt32(coordinates[0]), Convert.ToInt32(coordinates[1]), currentPlayer);
+					
+					//Обновление доступных ячеек
+					if (UpdateAvailableCells(myCell))
+					{
+						//Поиск победителя или взаимоблокировки (ничьи)
+						CheckWinner();
 
-				//Поиск победителя или взаимоблокировки (ничьи)
-				CheckWinner();
+						if (this.currentPlayer == Players.N || this.state != GameStates.Playing)
+						{
+							break;
+						}
+					}
+					else
+					{
+						Console.WriteLine("Данная ячейка уже заполнена, выберите другую!");
+					}
+
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("!!!НЕВЕРНО ВВЕДЕНЫ КООРДИНАТЫ!!!");
+				}
 			}
 
+			Console.Clear();
 			Console.WriteLine("Игра окончена!");
+			PrintGameField();
 
-			if (state == GameStates.Draw)
+			if (this.state == GameStates.Ended_With_Draw)
 			{
 				Console.WriteLine("Ничья!");
 			}
 			else
 			{
-				Console.WriteLine($"Победитель: {currentPlayer}!");
+				if (this.state == GameStates.Ended_With_X_Win)
+				{
+					winner = Players.X;
+				}
+				else
+				{
+					winner = Players.O;
+				}
+				Console.WriteLine($"Победитель: {winner}!");
 			}
 		}
 
@@ -160,19 +210,25 @@ namespace tic_tac_toe
 
 			for (int i = 0; i < gameField.Length; i++)
 			{
-				int[] line = gameField[i];
+				Cell[] line = gameField[i];
 
 				char[] charField = new char[3];
 
+				if (i == 0)
+				{
+					field.AppendLine("    0     1     2  ");
+					field.AppendLine("  _________________ ");
+				}
+
 				for (int j = 0; j < line.Length; j++)
 				{
-					int _ = line[j];
+					Cell cell = line[j];
 
-					if (_ == 0)
+					if (cell.Value == Players.N)
 					{
 						charField[j] = '-';
 					}
-					else if (_ == 1)
+					else if (cell.Value == Players.X)
 					{
 						charField[j] = 'X';
 					}
@@ -182,9 +238,9 @@ namespace tic_tac_toe
 					}
 				}
 
-				field.AppendLine("     |     |     ");
-				field.AppendLine(String.Format("  {0}  |  {1}  |  {2}  ", charField[0], charField[1], charField[2]));
-				field.AppendLine("_____|_____|_____");
+				field.AppendLine(" |     |     |     |");
+				field.AppendLine(String.Format("{3}|  {0}  |  {1}  |  {2}  |", charField[0], charField[1], charField[2], i));
+				field.AppendLine(" |_____|_____|_____|");
 			}
 
 			Console.WriteLine(field);
@@ -200,8 +256,7 @@ namespace tic_tac_toe
 			{
 				for (int j = 0; j < gameField[i].Length; j++)
 				{
-					Cell cell = new Cell(i, j, Players.N);
-					AvailableCellsList.Add(cell);
+					AvailableCellsList.Add(gameField[i][j]);
 				}
 			}
 		}
@@ -236,29 +291,233 @@ namespace tic_tac_toe
 		/// <summary>
 		/// Обновление списка доступных ячеек
 		/// </summary>
-		private void UpdateAvailableCells()
+		private bool UpdateAvailableCells(Cell myCell)
 		{
-			foreach (var cell in AvailableCellsList)
+			if (AvailableCellsList.Contains(new Cell(myCell.X, myCell.Y, Players.N)))
 			{
-				if (cell.Value != Players.N)
+				foreach (Cell cell in AvailableCellsList)
 				{
-					AvailableCellsList.Remove(cell);
+					if (cell.X == myCell.X && cell.Y == myCell.Y)
+					{
+						AvailableCellsList.Remove(cell);
+						break;
+					}
 				}
 			}
+
+			return gameField[myCell.X][myCell.Y].ChangeValue(myCell.Value);
 		}
 
+		/// <summary>
+		/// Нахождение победившего игрока или ничьи
+		/// </summary>
 		private void CheckWinner()
 		{
-			int countX, countY;
-
 			if (AvailableCellsList.Count < 1)
 			{
 				this.currentPlayer = Players.N;
+			}
 
-				//Проверка горизонтальных и вертикальных линий для ячейки [0,0]
+			//Проверка горизонтальных линий
+			Players winnerHorizontal = CheckHorizontal();
+			if (winnerHorizontal != Players.N)
+			{
+				if (winnerHorizontal == Players.X)
+				{
+					this.state = GameStates.Ended_With_X_Win;
+				}
+				else if (winnerHorizontal == Players.O)
+				{
+					this.state = GameStates.Ended_With_O_Win;
+				}
+
+				return;
+			}
+
+			//Проверка вертикальных линий
+			Players winnerVertical = CheckVertical();
+			if (winnerVertical != Players.N)
+			{
+				if (winnerHorizontal == Players.X)
+				{
+					this.state = GameStates.Ended_With_X_Win;
+				}
+				else if (winnerVertical == Players.O)
+				{
+					this.state = GameStates.Ended_With_O_Win;
+				}
+
+				return;
+			}
+
+			//Проверка диагональных линий
+			Players winnerDiagonal = CheckDiagonal();
+			if (winnerDiagonal != Players.N)
+			{
+				if (winnerDiagonal == Players.X)
+				{
+					this.state = GameStates.Ended_With_X_Win;
+				}
+				else if (winnerDiagonal == Players.O)
+				{
+					this.state = GameStates.Ended_With_O_Win;
+				}
+
+				return;
+			}
+
+			ChangePlayer();
+		}
+
+		/// <summary>
+		/// Проверка горизонтальных линий
+		/// </summary>
+		/// <returns>Ничья, Х или O в формате перечисления Players</returns>
+		private Players CheckHorizontal()
+		{
+			int countX = 0, countO = 0;
+
+			for (int i = 0; i < gameField.Length; i++)
+			{
+				for (int j = 0; j < gameField[i].Length; j++)
+				{
+					if (gameField[i][j].Value == Players.X)
+					{
+						countX++;
+					}
+					else if (gameField[i][j].Value == Players.O)
+					{
+						countO++;
+					}
+				}
+
+				if (countX == 3)
+				{
+					return Players.X;
+				}
+				else if (countX == 3)
+				{
+					return Players.O;
+				}
+
+				countX = 0;
+				countO = 0;
+			}
+
+			return Players.N;
+		}
+
+		/// <summary>
+		/// Проверка вертикальных линий
+		/// </summary>
+		/// <returns>Ничья, Х или O в формате перечисления Players</returns>
+		private Players CheckVertical()
+		{
+			int countX = 0, countO = 0;
+
+			//Прогонка игрового поля, как транспонированной матрицы i -> j, j -> i
+			for (int i = 0; i < gameField.Length; i++)
+			{
+				for (int j = 0; j < gameField[i].Length; j++)
+				{
+					if (gameField[j][i].Value == Players.X)
+					{
+						countX++;
+					}
+					else if (gameField[j][i].Value == Players.O)
+					{
+						countO++;
+					}
+				}
+
+				if (countX == 3)
+				{
+					return Players.X;
+				}
+				else if (countX == 3)
+				{
+					return Players.O;
+				}
+
+				countX = 0;
+				countO = 0;
+			}
+
+			return Players.N;
+		}
+
+		/// <summary>
+		/// Проверка двух диагональных линий
+		/// </summary>
+		/// <returns>Ничья, Х или O в формате перечисления Players</returns>
+		private Players CheckDiagonal()
+		{
+			int countX = 0, countO = 0;
+
+			//Проверка \ диагонали
+			for (int i = 0; i < gameField.Length; i++)
+			{
+				if (gameField[i][i].Value == Players.X)
+				{
+					countX++;
+				}
+				else if (gameField[i][i].Value == Players.O)
+				{
+					countO++;
+				}
+			}
+
+			if (countX == 3)
+			{
+				return Players.X;
+			}
+			else if (countX == 3)
+			{
+				return Players.O;
+			}
+
+			countO = 0;
+			countX = 0;
+
+			//Проверка / диагонали
+			for (int i = gameField.Length - 1; i >= 0; i--)
+			{
+				if (gameField[i][Math.Abs(i - 2)].Value == Players.X)
+				{
+					countX++;
+				}
+				else if (gameField[i][Math.Abs(i - 2)].Value == Players.O)
+				{
+					countO++;
+				}
 
 			}
 
+			if (countX == 3)
+			{
+				return Players.X;
+			}
+			else if (countX == 3)
+			{
+				return Players.O;
+			}
+
+			return Players.N;
+		}
+
+		/// <summary>
+		/// Метод перехода хода к другому игроку
+		/// </summary>
+		private void ChangePlayer()
+		{
+			if (currentPlayer == Players.X)
+			{
+				currentPlayer = Players.O;
+			}
+			else if (currentPlayer == Players.O)
+			{
+				currentPlayer = Players.X;
+			}
 		}
 
 		#endregion
